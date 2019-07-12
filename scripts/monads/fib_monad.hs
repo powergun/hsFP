c8cfb7470403d39c2334e196963635ef5c5700c3
Wei Ning <wei@canva.com>
2019-07-12 20:12:34 +1000

    wip

--- scripts/monads/fib_monad.hs
+++ scripts/monads/fib_monad.hs
@@ -1,22 +1,41 @@
 #!/usr/bin/env stack runghc
 
-import Control.Applicative
 import qualified Control.Monad.State as S
 import qualified Data.Map.Strict as M
+import FibMonadV1
 
-type FibMap a = M.Map a a
--- recall: State s a
-type FibState a b = S.State (FibMap a) b
+assert :: Bool -> IO ()
+assert True = do
+  return ()
+assert False = do
+  error "fail"
 
-getFib :: Integral a => a -> FibState a (Maybe a)
-getFib i = M.lookup i <$> S.get
+demoExecState :: IO () 
+demoExecState = do
+  print
+    "//////// demo execState() - no result, only the last state"
+  let mp = S.execState (fibWithState 30) M.empty
+      Just v = M.lookup 30 mp
+  assert $ 832040 == v
 
-putFib :: Integral a => a -> a -> FibState a a
-putFib i v = do
-  mp <- (pure (M.insert i v)) <*> S.get
-  put mp
-  return v
+demoEvalState :: IO ()
+demoEvalState = do
+  print
+    "//////// demo evalState() - throw away state; return result"
+  let mp = S.execState (fibWithState 30) M.empty
+  assert $ 514229 == (S.evalState (fibWithState 29) mp)
+  assert $ 0 == (S.evalState (fibWithState (-12)) mp)
 
-main :: IO () 
+demoRunState :: IO ()
+demoRunState = do
+  print
+    "//////// demo runState() - both the last state and result"
+  let o = S.runState (fibWithState 15) M.empty
+  assert $ 610 == (fst o)
+
+main :: IO ()
 main = do
-  print $ putFib 1 2
+  demoExecState
+  demoEvalState
+  demoRunState
+
