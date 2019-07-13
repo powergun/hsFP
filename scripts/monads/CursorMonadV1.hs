0875e7a262bea50d190e6cb33bec165dff883bae
Wei Ning <wei@canva.com>
2019-07-13 23:52:54 +1000

    wip

---
+++ scripts/monads/CursorMonadV1.hs
@@ -0,0 +1,37 @@
+module CursorMonadV1 
+  ( Cursor(..)
+  , Move(..)
+  , applyMoves
+  ) where 
+
+import qualified Control.Monad.State as S
+
+data Cursor = Cursor Int Int deriving (Eq, Show)
+
+data Move = North Int
+          | South Int
+          | West Int
+          | East Int
+          deriving Show
+
+apply :: Cursor -> Move -> Cursor
+apply (Cursor x y) (North i) = Cursor x (y - i)
+apply (Cursor x y) (South i) = Cursor x (y + i)
+apply (Cursor x y) (West i) = Cursor (x - i) y
+apply (Cursor x y) (East i) = Cursor (x + i) y
+
+-- L3235
+-- supposes we have many moves that need to be applied to the 
+-- cursor position. Use state monad, in which state the current
+-- position of the cursor is
+-- whenever we wish to apply a move to the cursor, we will get 
+-- the current cursor in the current state, apply the move 
+-- and then put the changed cursor back into the state
+-- MY NOTES:
+-- I have been looking for this pattern
+applyMoves :: [Move] -> S.State Cursor ()
+applyMoves [] = return ()
+applyMoves (x:xs) = do
+  cursor <- S.get
+  S.put (apply cursor x)
+  applyMoves xs
--- scripts/monads/cursor_monad_cook.hs
+++ scripts/monads/cursor_monad_cook.hs
@@ -3,47 +3,25 @@
 -- haskell cookbook L3221
 -- 
 
-import Prelude hiding (Left, Right)
+import CursorMonadV1
 import qualified Control.Monad.State as S
 
-data Cursor = Cursor Int Int deriving Show
-
-data Move = Up Int
-          | Down Int
-          | Left Int
-          | Right Int
-          deriving Show
-
-apply :: Cursor -> Move -> Cursor
-apply (Cursor x y) (Up i) = Cursor x (y - i)
-apply (Cursor x y) (Down i) = Cursor x (y + i)
-apply (Cursor x y) (Left i) = Cursor (x - i) y
-apply (Cursor x y) (Right i) = Cursor (x + i) y
-
--- L3235
--- supposes we have many moves that need to be applied to the 
--- cursor position. Use state monad, in which state the current
--- position of the cursor is
--- whenever we wish to apply a move to the cursor, we will get 
--- the current cursor in the current state, apply the move 
--- and then put the changed cursor back into the state
--- MY NOTES:
--- I have been looking for this pattern
-applyMoves :: [Move] -> S.State Cursor ()
-applyMoves [] = return ()
-applyMoves (x:xs) = do
-  cursor <- S.get
-  S.put (apply cursor x)
-  applyMoves xs
+assert :: Bool -> IO ()
+assert True = do
+  return ()
+assert False = do
+  error "fail"
 
 demoMoves :: IO ()
 demoMoves = do
-  let moves = [ Down 100
-              , Right 100
-              , Up 100
-              , Left 100 ]
-  print $ S.execState (applyMoves moves) (Cursor 0 0)
-  print $ S.execState (applyMoves moves) (Cursor 12 12)
+  let moves = [ South 100
+              , East 100
+              , North 100
+              , West 100 ]
+      c1 = S.execState (applyMoves moves) (Cursor 0 0)
+      c2 = S.execState (applyMoves moves) (Cursor 12 12)
+  assert $ Cursor 0 0 == c1
+  assert $ Cursor 12 12 == c2
 
 main :: IO ()
 main = do
