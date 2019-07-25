bdefab2549e25d5360d5784998410a3c3bd6a302
Wei Ning <macgnw@gmail.com>
2019-07-26 07:44:29 +1000

    wip

--- scripts/monads/polymorphic_monad.hs
+++ scripts/monads/polymorphic_monad.hs
@@ -1,10 +1,17 @@
 #!/usr/bin/env stack runghc
 
 -- inspired by video Monad in less than 10 minutes
+
 -- we can write code that is polymorphic in our choice of Monad
+
+-- foldM works with list monad and maybe monad; I don't have to 
+-- worry about the process of folding, but instead I can focus
+-- on the lambda part that talks to the pure value
 import Control.Monad
 main :: IO ()
 main = do
-  result <- foldM (\s e -> return e) 0 [1]
-  print result
+  r1 <- foldM (\s e -> return (s + e)) 0 [1, 3, 4, 5]
+  print r1
 
+  r2 <- foldM (\s e -> return (s ++ e)) "there" (Just "is")
+  print r2
