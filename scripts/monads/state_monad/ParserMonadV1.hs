e4f932a9fac4b7ad2d4d9892487b75238457f49c
Wei Ning <macgnw@gmail.com>
2019-08-15 20:14:04 +1000

    working on ParserMonad reference impl

--- scripts/monads/state_monad/ParserMonadV1.hs
+++ scripts/monads/state_monad/ParserMonadV1.hs
@@ -1,6 +1,33 @@
 module ParserMonadV1
   ( Parser(..)
-
+  , char
+  , charUpper
   ) where
 
+import           Data.Char (toUpper)
+
+-- programming haskell L5122
+
 newtype Parser a = Parser { parse :: String -> [(a, String)] }
+
+char :: Char -> Parser Char
+char c = Parser f
+  where
+    f [] = []
+    f (x : xs) | x == c    = [(x, xs)]
+               | otherwise = []
+
+{-
+recall Brian Beckman's video: functor and monad is to enable
+us to compose complex function from simple functions in an
+othorgnal way
+-}
+
+instance Functor Parser where
+  fmap f p = Parser f'
+    where
+      f' string = case parse p string of
+                    []         -> []
+                    [(v, out)] -> [(f v, out)]
+
+charUpper c = toUpper <$> char c
--- scripts/monads/state_monad/ParserMonadVTest.hs
+++ scripts/monads/state_monad/ParserMonadVTest.hs
@@ -1,8 +1,25 @@
 #!/usr/bin/env stack runghc
 
+import           Data.Char
+
 import           ParserMonadV1
 
+assert :: Bool -> IO ()
+assert True =
+  return ()
+assert False =
+  error "fail"
+
+testParsingOneChar :: IO ()
+testParsingOneChar = do
+  assert $ parse (char 't') "there" == [('t', "here")]
+  assert $ null (parse (char 't') "iddqd")
+
+testFunctor :: IO ()
+testFunctor = do
+  assert $ parse (charUpper 't') "there" == [('T', "here")]
+  assert $ parse (toLower <$> char 'I') "ID" == [('i', "D")]
+
 main :: IO ()
-main = do
-  let p = Parser (\_ -> [(' ', "")])
-  print $ parse p ""
+main =
+  testParsingOneChar
