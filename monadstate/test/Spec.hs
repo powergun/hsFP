9c49c8a8e5d5f3ad6e8d1871a3a0efae8c5335dc
Wei Ning <wei@canva.com>
2019-11-20 14:57:58 +1100

    wip

--- monadstate/test/Spec.hs
+++ monadstate/test/Spec.hs
@@ -1,7 +1,6 @@
 import qualified TestCommands
 import qualified TestMoveCursor
 import qualified TestFibonacci
-import qualified TestSimpleTransformer
 import qualified TestSimpleState
 import qualified TestGameState
 
@@ -10,6 +9,5 @@ main = do
   TestCommands.runSpec
   TestMoveCursor.runSpec
   TestFibonacci.runSpec
-  TestSimpleTransformer.runSpec
   TestSimpleState.runSpec
   TestGameState.runSpec
