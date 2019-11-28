01e09e16989e1bfde90ebc87c0764af0222f85a6
Wei Ning <wei@canva.com>
2019-11-28 16:52:48 +1100

    wip

--- functors/src/Bifunctors.hs
+++ functors/src/Bifunctors.hs
@@ -2,8 +2,21 @@ module Bifunctors where
 
 import qualified Data.Bifunctor as DB
 
-data GameSession = Either Int Int
+type GameSession = Either Int Int
 
+demoFirstSecond :: IO ()
+demoFirstSecond = do
+  let gs :: GameSession
+      gs = Left (-10)
+      gs' :: GameSession
+      gs' = Right (99)
+  case (DB.first show gs) of
+    Left v -> print $ "Left(error): " ++ v
+    _ -> return ()
+  case (DB.second show gs') of
+    Right v -> print $ "Right(success): " ++ v
+    _ -> return ()
+  
 demo :: IO ()
 demo = do
-  return ()
+  demoFirstSecond
