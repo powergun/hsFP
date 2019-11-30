ab616fab75c255209cdd5aaf86fa5a6e50028bfb
Wei Ning <wei@canva.com>
2019-11-30 15:48:42 +1100

    wip

--- exception/test/Spec.hs
+++ exception/test/Spec.hs
@@ -1,9 +1,10 @@
 import qualified EitherTry
-
 import qualified TwoFilesProblem
+import qualified ThrowError
 
 main :: IO ()
 main = do
   EitherTry.demo
   TwoFilesProblem.demo ("./testdata/file1", "./testdata/file1")
   TwoFilesProblem.demo ("./testdata/file1", "./testdata/file12")
+  ThrowError.demo
--- monadreader/src/Parametric/Demo.hs
+++ monadreader/src/Parametric/Demo.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 
+-- source
+-- https://thoughtbot.com/blog/refactoring-to-a-monad-transformer-stack
+-- this is an INCOMPLETE demo, aiming to exhibit the parametric
+-- MonadReader 
+-- but it is UNCLEAR to me what the benefit/usage is
+
 module Parametric.Demo where
 
 import Data.Bool (bool)
